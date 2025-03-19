f2e71873cb83a1a04ab0bab42c50812d410ac200
shubhamjuyal <shubhamjuyal4444@gmail.com>
2025-03-20 02:35:06 +0530

    Chat Component

---
+++ vixdom/src/app/Chat.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const Chat = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen space-y-4 border-8">
+      In Progress!
+    </div>
+  );
+};
--- vixdom/src/app/page.tsx
+++ vixdom/src/app/page.tsx
@@ -1,29 +1,32 @@
 "use client";
 import Home from "./Home";
-import { useCurrentState, StateContextType } from "./StateContext";
+import { useCurrentState } from "./StateContext";
 import { Icons } from "../components/icons/Icons";
+import { Chat } from "./Chat";
 
 export default function Page() {
-  const currentState: StateContextType | undefined = useCurrentState();
-  const showContent = () => {
-    if (!currentState?.currentState) {
-      return <Home />;
-    } else if (currentState?.currentState == 1) {
-      return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
-          <Icons.spinner className="h-16 w-16 animate-spin text-gray-600" />
-        </div>
-      );
-    } else if (currentState?.currentState == 2) {
-      return <div>{currentState.file.name} hi kehde!</div>;
+  const { currentState } = useCurrentState() || {}; // Destructuring for better readability
+
+  const renderContent = () => {
+    switch (currentState) {
+      case 1:
+        return (
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <Icons.spinner className="h-16 w-16 animate-spin text-gray-600" />
+          </div>
+        );
+      case 2:
+        return <Chat />;
+      default:
+        return (
+          <div className="border-4 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+              <Home />
+            </div>
+          </div>
+        );
     }
   };
 
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {showContent()}
-      </main>
-    </div>
-  );
+  return <main>{renderContent()}</main>;
 }
