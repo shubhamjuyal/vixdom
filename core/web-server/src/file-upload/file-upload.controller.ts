32513797ec1bdcd60c445650be10ceb9dd93035e
shubhamjuyal <shubhamjuyal4444@gmail.com>
2025-03-22 19:09:35 +0530

    file-upload API

--- core/web-server/src/file-upload/file-upload.controller.ts
+++ core/web-server/src/file-upload/file-upload.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+  BadRequestException,
+} from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { File as MulterFile } from "multer";
 
 @Controller("file-upload")
-export class FileUploadController {}
+export class FileUploadController {
+  @Post()
+  @UseInterceptors(
+    FileInterceptor("file", {
+      fileFilter: (req, file, callback) => {
+        // Validate for csv
+        if (!file.originalname.match(/\.(csv)$/)) {
+          return callback(
+            new BadRequestException("Only CSV files are allowed!"),
+            false
+          );
+        }
+        callback(null, true);
+      },
+    })
+  )
+  uploadFile(@UploadedFile() file: MulterFile) {
+    if (!file) {
+      throw new BadRequestException("File is not provided");
+    }
+    const fileContent = file.buffer.toString("utf8");
+    const firstLine = fileContent.split(/\r?\n/)[0];
+    const headers = firstLine.split(",").map((header) => header.trim());
+
+    return {
+      message: "File upload successfully.",
+      fileName: file.originalname,
+      headers: headers.map((_) => {
+        return { name: _, dataType: "Text" };
+      }),
+    };
+  }
+}
