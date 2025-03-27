8fa5aab47606b387f712466184da83ba536cf517
shubhamjuyal <shubhamjuyal4444@gmail.com>
2025-03-27 23:21:31 +0530

    Update chat.controller.ts

--- core/web-server/src/chat/chat.controller.ts
+++ core/web-server/src/chat/chat.controller.ts
@@ -1,15 +1,34 @@
-import { Body, Controller, Post, Res } from "@nestjs/common";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Post,
+  Res,
+  UploadedFile,
+} from "@nestjs/common";
 import { Response } from "express";
 import { ChatService } from "./chat.service";
 import { ChatDto } from "./chat.dto";
+import { File as MulterFile } from "multer";
 
 @Controller("chat")
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Post("/")
-  async uploadFile(@Body() chatDto: ChatDto, @Res() res: Response) {
-    const streamingResponse = await this.chatService.chat(chatDto);
+  async uploadFile(
+    @UploadedFile() file: MulterFile,
+    @Body() chatDto: ChatDto,
+    @Res() res: Response
+  ) {
+    // console.log("chatDto: ", chatDto);
+    // // extract file data
+    // const fileData = this.chatService.extractSheetData(file);
+    // if (!fileData) throw new BadRequestException("File is not provided");
+    // //return stram response
+    const streamingResponse = await this.chatService.chat(
+      chatDto /*, fileData*/
+    );
     res.setHeader("Content-Type", "text/event-stream");
     if (streamingResponse.body) {
       streamingResponse.body.pipeTo(
